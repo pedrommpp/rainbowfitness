cf9dba7f3885be538f57a9e24051230bb29b4f5c
pedrommpp <pedro2pasos@gmail.com>
2026-03-23 16:34:37 -0300

    Refactor service worker for caching assets

--- app/sw.js
+++ app/sw.js
@@ -1,10 +1,24 @@
-const CACHE_NAME = "rainbowfitness-v1";
-const urlsToCache = ["/", "/index.html"];
+const CACHE_NAME = 'rainbowfitness-v1';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json'
+];
 
-self.addEventListener("install", event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+// Instala o Service Worker e guarda no cache
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request)));
+// Faz o app carregar os arquivos do cache quando estiver sem internet
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
